b67012ee8c0b20178ba72bbd13ec144d95582e4e
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-05 21:57:51 +0530

    questions

--- functions/basics/script.js
+++ functions/basics/script.js
@@ -1,4 +1,6 @@
 let prompt = require('prompt-sync')()
 
-let age = prompt("enter your age : ")
+let age = +(prompt("enter your age : "))
+console.log(typeof age );
+
 console.log(`the age is ${age}`);
