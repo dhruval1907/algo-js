b1c3f008b396a703ccaa71722595fec294359de9
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-05 22:24:21 +0530

    finished

--- functions/questions/swapnumbers/script.js
+++ functions/questions/swapnumbers/script.js
@@ -1,6 +1,7 @@
 // swap two numbers
 
 console.log("fisrt method");
+console.log("------------------------------------------");
 
 let a = 20;
 let b = 30;
@@ -15,7 +16,24 @@ console.log(`the swap number is ${b}`);
 console.log("second method");
 console.log("------------------------------------------");
 
+let x = 38
+let y = 92
+
+x = x + y
+y = x - y
+x = x - y
+
+console.log(`the swap number is ${x}`);
+console.log(`the swap number is ${y}`);
 
 
+console.log("third method");
+console.log("------------------------------------------");
+let g = 10;
+let h = 20;
 
+[g,h] = [h,g];
+
+console.log(g);
+console.log(h);
 
