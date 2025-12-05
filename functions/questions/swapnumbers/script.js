cc48220b16f7250482a8a8415cb4233e41b291d7
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-05 22:11:33 +0530

    swappinigmethos

---
+++ functions/questions/swapnumbers/script.js
@@ -0,0 +1,21 @@
+// swap two numbers
+
+console.log("fisrt method");
+
+let a = 20;
+let b = 30;
+let c = a;
+
+a = b;
+b = c;
+
+console.log(`the swap number is ${a}`);
+console.log(`the swap number is ${b}`);
+
+console.log("second method");
+console.log("------------------------------------------");
+
+
+
+
+
