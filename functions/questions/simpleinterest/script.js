dce793ed8f135d2346ba1078c43cd1aa6384c518
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-07 20:41:34 +0530

    functions

--- functions/questions/simpleinterest/script.js
+++ functions/questions/simpleinterest/script.js
@@ -1,7 +1,15 @@
-let prompt = require("prompt-sync")()
+// let prompt = require("prompt-sync")()
 
-let p = prompt("enter ur principle : ")
-let r = prompt("enter rate of interest : ")
-let t = prompt("enter time : ")
+// let p = prompt("enter ur principle : ")
+// let r = prompt("enter rate of interest : ")
+// let t = prompt("enter time : ")
 
-console.log((p * Math.pow(1 + (r / 100), t) - p).toFixed(2));
+// console.log((p * Math.pow(1 + (r / 100), t) - p).toFixed(2));
+
+// otp generator
+
+// console.log(Math.floor(Math.random()*9999)+1000);
+
+pi = Math.PI
+console.log(pi);
+
