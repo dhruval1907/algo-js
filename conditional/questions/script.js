bf1582688768bf3207e30bdf58d648650b0ff44c
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-08 23:34:51 +0530

    quesionssss

--- conditional/questions/script.js
+++ conditional/questions/script.js
@@ -32,11 +32,11 @@ if(unit>0 && unit<=100){
     console.log(unit*4.2);
 }
 else if (unit>=100 && unit<=200){
-    console.log((unit*4.2)+((unit-100)*6));
+    console.log((100*4.2)+(unit-100)*6);
 }
 else if (unit>=200 && unit<=300){
-    console.log((unit*4.2)+(unit*4.2)+((unit-200)*8));
+    console.log((100*4.2)+(100*6)+(unit-200)*8);
 }
 else{
-    console.log((unit*4.2)+(unit*4.2)+((unit*8))+((unit-300)*13));
+    console.log((100*4.2)+(100*6)+(200*8)+(unit-400)*13);
 }
