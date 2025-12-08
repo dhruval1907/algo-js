33cfec88ab0b875a7e6113e24dce2e185234f3fa
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-08 23:28:17 +0530

    quesionssss

--- conditional/questions/script.js
+++ conditional/questions/script.js
@@ -12,15 +12,31 @@
 // }
 
 
+// let prompt = require('prompt-sync')()
+// let amount = +(prompt("enter your amount "));
+
+// let dis = 0
+// if(amount>0 && amount <5000 ) dis =0
+// else if(amount>5000 && amount <7000 ) dis =5
+// else if(amount>7000 && amount <9000 ) dis =10
+// else dis=20
+
+// let payableAmount = amount - ((dis*amount)/100)
+
+// console.log(payableAmount);
 let prompt = require('prompt-sync')()
-let amount = +(prompt("enter your amount "));
 
-let dis = 0
-if(amount>0 && amount <5000 ) dis =0
-else if(amount>5000 && amount <7000 ) dis =5
-else if(amount>7000 && amount <9000 ) dis =10
-else dis=20
+let unit = +(prompt("enter your unit "));
 
-let payableAmount = amount - ((dis*amount)/100)
-
-console.log(payableAmount);
+if(unit>0 && unit<=100){
+    console.log(unit*4.2);
+}
+else if (unit>=100 && unit<=200){
+    console.log((unit*4.2)+((unit-100)*6));
+}
+else if (unit>=200 && unit<=300){
+    console.log((unit*4.2)+(unit*4.2)+((unit-200)*8));
+}
+else{
+    console.log((unit*4.2)+(unit*4.2)+((unit*8))+((unit-300)*13));
+}
